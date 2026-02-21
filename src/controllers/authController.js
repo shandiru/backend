d19514a27b6c849b9349c92dc17bec90f5a8cfbf
shandiru <msddiru@gmail.com>
2026-02-21 22:43:27 +0530

    added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -130,7 +130,7 @@ export const logoutUser = async (req, res) => {
 
  export const fetachAllUsers = async (req, res) => {
   try {
-    const users = await User.find({}, 'firstName lastName email role'); // Fetch only specific fields
+    const users = await User.find({}, 'firstName lastName email role  isActive'); // Fetch only specific fields
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
