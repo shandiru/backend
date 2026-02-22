81ed98449639b922a3a54bc9f8537ce2ac5da2ab
shandiru <msddiru@gmail.com>
2026-02-22 20:22:18 +0530

    added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -127,7 +127,7 @@ export const logoutUser = async (req, res) => {
   res.clearCookie("refreshToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-     sameSite: 'None',
+     sameSite: 'Strict',
   });
    console.log("Refresh token cookie cleared"); // Debugging line
   res.status(200).json({ message: "Logged out successfully" });
