639116d651932f809b2df067b8bcf43997f3ec48
shandiru <msddiru@gmail.com>
2026-02-22 22:30:12 +0530

    added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://admin-two-xi-55.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
